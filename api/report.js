cdbabf6039bc40d95f181dfbd17faf0aab1edbf2
itsfelipeh <itsfelipeh@itsfelipeh.com>
2025-07-30 16:37:54 -0400

    Update report.js

--- api/report.js
+++ api/report.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
       longitud
     } = req.body;
 
-    const scriptURL = "https://script.google.com/macros/s/AKfycbyxey2tSSEwwe1ht_MlNItLEm81MKU-rv0vi1XBzOLAnM51UUdRwlrvsTF4GWpEyT4c/exec";
+    const scriptURL = "https://script.google.com/macros/s/AKfycbwkx6tdqQsiMRMZXwObA9Cizot_bAZPgttklNOJ_nUSTnWkoET-7hETDjW7YvZ8PuOohw/exec";
 
     const response = await fetch(scriptURL, {
       method: "POST",
